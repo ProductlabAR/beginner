63ead3c4ebdedd53202cb0bce9c1afde9d78439f
ProductlabAR <dev@productlab.it>
2015-08-23 10:40:17 +0200

    f u

--- beginner/Main.cs
+++ beginner/Main.cs
@@ -2,11 +2,87 @@ using System;
 
 namespace Beginner
 {
-	class MainClass
+	class Beginer
 	{
-		public static void Main (string[] args)
+		static void Main()
 		{
-			Console.WriteLine ("Hello Noo!");
+			Noob greg = new Noob();
+			Console.WriteLine("Greg start counter = " + greg.StartCounter);
+			greg.StartCounter = 2;
+			Console.WriteLine("Greg start counter = " + greg.StartCounter);
+			greg.EndCounter = 6;
+			greg.PrintName();
+			Console.ReadLine();
 		}
+	}
+	
+	class Noob
+	{
+		private int startCounter = 8;
+		private int endCounter;
+		
+		public int StartCounter
+		{
+			get
+			{
+				return startCounter;
+			}
+			set
+			{
+				startCounter = value;
+			}
+		}
+		
+		public int EndCounter
+		{
+			get
+			{
+				return endCounter;
+			}
+			set
+			{
+				endCounter = value;
+			}
+		}
+		
+		//public Noob(int start, int end)
+		//{
+		//	startCounter = start;
+		//	endCounter = end;
+		//}
+		
+		public static void PrintS(string p1, string p2)
+		{
+			Console.WriteLine(p1 + p2);
+		}
+		
+		public void Print(string p1, string p2)
+		{
+			PrintPrivate(p1, p2);
+		}
+		
+		private void PrintPrivate(string p1, string p2)
+		{
+			Console.WriteLine(p1 + p2);
+		}
+		
+		public void PrintName()
+		{
+			string name;
+			Console.Write("Input name please: ");
+			name = Console.ReadLine();
+			if(name.ToLower() != "fuck you" && startCounter <= endCounter )
+			{
+				Console.WriteLine("Your name is: " + name);
+				startCounter ++;
+				PrintName();
+			}
+			else
+			{
+				Console.WriteLine("Fuck YOU");
+			}
+		}
+		
+		
 	}
 }
